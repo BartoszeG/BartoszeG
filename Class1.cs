b258229f86cc9300039403301016b817c71817f1
BartoszeG <92253526+BartoszeG@users.noreply.github.com>
2022-01-15 17:36:36 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie9
+{
+    class ProgramEnum
+    {
+        enum MyEnum
+        {
+            PKP,
+            INTERCITY,
+            EDUKACJA,
+            NAUKOWA,
+            INFORMATYCZNA,
+            KLASYCZNA,
+        }
+        public static void solution()
+        {
+            int namesCount = Enum.GetNames(typeof(MyEnum)).Length;
+            for (int i = 0; i < namesCount; i++)
+            {
+                Console.WriteLine($"Enum:{Enum.GetNames(typeof(MyEnum))[i]} Wartość: {i}");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zadanie9
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgramEnum.solution();
+            Console.ReadKey();
+
+        }
+    }
+}
